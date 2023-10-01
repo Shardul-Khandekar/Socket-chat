620dbc4df8e8bb67179d34d5c8df8df222e7e5cc
unknown <khandekarshardul@gmail.com>
2023-10-01 16:00:24 -0400

    read port value from env

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import 'dotenv/config';
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Homepage")
+})
 
 app.listen(PORT, () => {
     console.log("Server running on port: " + PORT);
